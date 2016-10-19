0215dcca9e984e6cc0c75eee208c5e5c231c81aa
olafvdg <olaf@brinkerink.nl>
2016-10-19 14:18:58 +0200

    Fitst commit

---
+++ js/controllers.js
@@ -0,0 +1,82 @@
+angular.module('vwrl.controllers', [])
+
+.filter('pipetobreak', function() {
+	return function(input) {
+		return input.replace(/\|/g, '<br/>');
+	}
+})
+
+.controller('LoginCtrl', function($rootScope, $scope, $timeout, $state, $ionicHistory, Checklist) {
+
+  $scope.doLogin = function(loginData) {
+    //console.log('Doing login');
+    Checklist.GetChecks();
+    $timeout(function() {
+	  //console.log(md5(loginData.password));
+	  if(md5(loginData.password) == $rootScope.pw) {
+		  // Store in database
+		  $rootScope.store.save({key:'app-pw',value: $rootScope.pw});
+		  $ionicHistory.nextViewOptions({ historyRoot: true })
+		  window.location = '#/app/disclaimer';
+	  } else {
+		  console.log('Fout...');
+	  }
+    }, 1000);
+  };
+})
+
+.controller('CheckCtrl', function($stateParams,$scope,Checklist) {
+    var checkId = $stateParams.id;
+    //console.log(checkId);
+    if(Checklist.IsAvailable()) {
+      $scope.check = Checklist.GetCheck(checkId);
+    } else {
+      Checklist.GetChecks().then(function() {
+        $scope.check = Checklist.GetCheck(checkId);
+      });
+    }
+})
+
+.controller('ChecksCtrl', function($stateParams,$scope,Checklist) {
+    var cat = $stateParams.cat;
+    var subcat = $stateParams.subcat;
+    if(Checklist.IsAvailable()) {
+      $scope.checks = Checklist.GetCat(cat,subcat);
+    } else {
+      Checklist.GetChecks().then(function() {
+        $scope.checks = Checklist.GetCat(cat,subcat);
+      });
+    }
+    $scope.CAT = cat;
+    Checklist.unCheck();
+    $scope.boxChecked = function() {
+      var count = 0;
+      //console.log($scope.checks[0]);
+       for(i=0;i<$scope.checks.length;i++){
+                if($scope.checks[i].checked){
+                    count = count + 1;
+                }
+        }
+        if(count == $scope.checks.length ) {
+          if($scope.checks[0].NCAT=='') {
+            window.location = '#/app/tussenmenu';
+          } else {
+            window.location = '#/app/checks/'+$scope.checks[0].NCAT;
+          }
+        }
+    };
+})
+
+.controller('PlaylistsCtrl', function($scope) {
+  $scope.playlists = [
+    { title: 'Reggae', id: 1 },
+    { title: 'Chill', id: 2 },
+    { title: 'Dubstep', id: 3 },
+    { title: 'Indie', id: 4 },
+    { title: 'Rap', id: 5 },
+    { title: 'Cowbell', id: 6 }
+  ];
+})
+
+.controller('PlaylistCtrl', function($scope, $stateParams) {
+});
